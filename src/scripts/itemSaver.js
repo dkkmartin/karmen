5a1e0c53727f94dfb8a7805b29267848b8b6b8e1
Martin Bruun <martinbruun1995@gmail.com>
2023-05-25 14:05:03 +0200

    Add: js for itemSaver

--- src/scripts/itemSaver.js
+++ src/scripts/itemSaver.js
@@ -1,3 +1,48 @@
-//TODO 
+// TODO
 // Put in basket saves the product in localstorage as KURV
 // Localstorage KURV holds the product, price, pieces
+const btn = document.querySelector('button')
+const staffeli = {
+  name: 'staffeli',
+  price: '30',
+  count: '0'
+}
+const sukkulent = {
+  name: 'sukkulent',
+  price: '50',
+  count: '0'
+}
+const kurv = {
+  name: 'kurv',
+  price: '30',
+  count: '0'
+}
+const ugle = {
+  name: 'ugle',
+  price: '50',
+  count: '0'
+}
+const krokodille = {
+  name: 'krokodille',
+  price: '65',
+  count: '0'
+}
+
+btn.addEventListener('click', () => {
+  if (document.URL.includes('/staffeli/')) {
+    staffeli.count = staffeli.count + 1
+    localStorage.setItem('staffeli', JSON.stringify(staffeli))
+  }
+  if (document.URL.includes('/sukkulent/')) {
+    localStorage.setItem('staffeli', JSON.stringify(sukkulent))
+  }
+  if (document.URL.includes('/kurv/')) {
+    localStorage.setItem('staffeli', JSON.stringify(kurv))
+  }
+  if (document.URL.includes('/ugle/')) {
+    localStorage.setItem('staffeli', JSON.stringify(ugle))
+  }
+  if (document.URL.includes('/krokodille/')) {
+    localStorage.setItem('staffeli', JSON.stringify(krokodille))
+  }
+})
