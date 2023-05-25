ab0010ad403d19c6c2b972d79be8d8ef3513d482
Martin <martinbruun1995@gmail.com>
2023-05-26 01:29:23 +0200

    Add: Kurv JS logic

--- src/scripts/itemSaver.js
+++ src/scripts/itemSaver.js
@@ -5,34 +5,85 @@ import { showKurv, updateKurvWithProducts } from './kurv'
 
 const btn = document.querySelector('button')
 
+window.onload = function () {
+  if (localStorage.getItem('hasCodeRunBefore') === null) {
+    const staffeli = {
+      name: 'Mini staffeli',
+      price: 30,
+      count: 0,
+      alt: 'Inkl. malerlærred',
+      src: '/public/staffeli.png'
+    }
+    localStorage.setItem('staffeli', JSON.stringify(staffeli))
+    const sukkulent = {
+      name: 'Sukkulent',
+      price: 30,
+      count: 0,
+      alt: 'Grøn plante',
+      src: '/public/plante_3.png'
+    }
+    localStorage.setItem('sukkulent', JSON.stringify(sukkulent))
+    const kurv = {
+      name: 'Kurv',
+      price: 30,
+      count: 0,
+      alt: 'Kurv af træ',
+      src: '/public/kurv.png'
+    }
+    localStorage.setItem('kurv', JSON.stringify(kurv))
+    const ugle = {
+      name: 'Ugle',
+      price: 30,
+      count: 0,
+      alt: 'Ugle af træ',
+      src: '/public/ugle.png'
+    }
+    localStorage.setItem('ugle', JSON.stringify(ugle))
+    const krokodille = {
+      name: 'Krokodille',
+      price: 65,
+      count: 0,
+      alt: 'Krokodille af træ',
+      src: '/public/krokodille.png'
+    }
+    localStorage.setItem('krokodille', JSON.stringify(krokodille))
+    localStorage.setItem('hasCodeRunBefore', true)
+  }
+}
+
 btn.addEventListener('click', () => {
   if (document.URL.includes('/staffeli/')) {
-    staffeli.count++
-    localStorage.setItem('staffeli', JSON.stringify(staffeli))
+    const LSObject = JSON.parse(localStorage.getItem('staffeli'))
+    LSObject.count++
+    localStorage.setItem('staffeli', JSON.stringify(LSObject))
     updateKurvWithProducts()
     showKurv()
   }
   if (document.URL.includes('/sukkulent/')) {
-    sukkulent.count++
-    localStorage.setItem('sukkulent', JSON.stringify(sukkulent))
+    const LSObject = JSON.parse(localStorage.getItem('sukkulent'))
+    LSObject.count++
+    localStorage.setItem('sukkulent', JSON.stringify(LSObject))
     updateKurvWithProducts()
     showKurv()
   }
   if (document.URL.includes('/kurv/')) {
-    kurv.count++
-    localStorage.setItem('kurv', JSON.stringify(kurv))
+    const LSObject = JSON.parse(localStorage.getItem('kurv'))
+    LSObject.count++
+    localStorage.setItem('kurv', JSON.stringify(LSObject))
     updateKurvWithProducts()
     showKurv()
   }
   if (document.URL.includes('/ugle/')) {
-    ugle.count++
-    localStorage.setItem('ugle', JSON.stringify(ugle))
+    const LSObject = JSON.parse(localStorage.getItem('ugle'))
+    LSObject.count++
+    localStorage.setItem('ugle', JSON.stringify(LSObject))
     updateKurvWithProducts()
     showKurv()
   }
   if (document.URL.includes('/krokodille/')) {
-    krokodille.count++
-    localStorage.setItem('krokodille', JSON.stringify(krokodille))
+    const LSObject = JSON.parse(localStorage.getItem('krokodille'))
+    LSObject.count++
+    localStorage.setItem('krokodille', JSON.stringify(LSObject))
     updateKurvWithProducts()
     showKurv()
   }
